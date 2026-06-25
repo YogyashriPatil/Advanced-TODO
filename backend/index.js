04b9b8e915a4b9e5f413fc258c3a6459e2023c19
YogyashriPatil <yogyashripatil20@gmail.com>
2026-06-25 16:12:17 +0530

    Created authentication routes

--- backend/index.js
+++ backend/index.js
@@ -4,22 +4,93 @@ import express from "express";
 import { configDotenv } from "dotenv";
 const app = express();
 configDotenv();
-app.get("/signin",(req,res) => {
+app.use(express.json())
+
+let users = []
+let userid = 0;
+let todoid=0;
+app.post("/signup", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
-
     
-
-})
-
-app.get("/",(req,res) => {
+    const exitingUser = users.find(u => u.name === username && u.password === password)
+    console.log(exitingUser)
+    if(exitingUser){
+        res.status(400).json({
+            message: "User is already register",
+            id: exitingUser.id
+        })
+    }
+    else {
+        users.push({
+            'id': ++userid,
+            'name':username,
+            'password':password,
+            'todo':[]
+        })
+        res.json({
+            message: users
+        })
+    }
     
 })
 
-app.get("/",(req,res) => {
+app.post("/signin", (req,res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    
+    const userExits = users.find(u => u.name === username && u.password===password);
+    if(!userExits){
+        res.status(400).json({
+            message:"User not exist"
+        })
+    }
+    else{
+        res.json({
+            message:"Successfully signin",
+            id:userExits.id
+        })
+    }
+
+})
+
+app.get("/profile/:id", (req,res) => {
+    const id = Number(req.params.id);
+    console.log(id);
+    const userInfo = users.find(u => u.id === id);
+    console.log(userInfo)
+    res.json({
+        message: userInfo
+    })
     
 })
 
-app.listen(process.env.PORT, (req,res) => {
-    console.log(`Listening on the ${process.env.PORT}`);
+app.post("/create-todo/:id", (req,res) => {
+    const id = Number(req.params.id);
+    const user = users.find(u => u.id === id);
+    console.log(user);
+    if (!user) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+    const todoName = req.body.todoName;
+    const todoDesc = req.body.todoDesc;
+    const todoStartTime = req.body.todoStartTime;
+    const todoEndTime = req.body.todoEndTime;
+    user.todo.push({
+        'id':++todoid,
+        'todoName':todoName,
+        'todoDesc': todoDesc,
+        'todoStartTime': todoStartTime,
+        'todoEndTime': todoEndTime,
+        'status':'incomplete'
+    })
+    res.json({
+        message:"Todo added"
+    })
 })
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening on port ${process.env.PORT}`);
+});
